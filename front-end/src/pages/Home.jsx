d961d0e842f04b7251ca26f3d9af4d5737276ff9
khalidesskali <esskalikhalid7@gmail.com>
2025-06-21 00:50:44 +0100

    Add AddPost component to Home page layout

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../layouts/Header";
+import AddPost from "../components/AddPost";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <AddPost />
     </div>
   );
 };
