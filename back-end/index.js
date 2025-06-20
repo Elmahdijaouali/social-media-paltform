d6451d3d08bf1c84cf2a0329160576036ec02fbd
KaiserOfTheNight <abderrazzak.elouazghi@gmail.com>
2025-06-20 21:54:48 +0200

    add posts

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,19 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
-dotenv.config()
+import mongoose from 'mongoose';
+import postRoute from './routes/postRoute.js';
 
-const PORT = process.env.PORT || 8000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const app = express()
+app.use(express.json()); 
 
+mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .catch((error) => console.log(error.message));
 
+// Routes
+app.use('/posts', postRoute);
 
-app.listen(PORT , () => {
-    console.log(`Server is running on port ${PORT}`)
-})
