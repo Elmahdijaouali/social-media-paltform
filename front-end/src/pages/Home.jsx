6ae596e089b2dc815dd39b6089a99414a31642a7
khalidesskali <esskalikhalid7@gmail.com>
2025-06-21 13:40:49 +0100

    Add Posts component to Home page and update layout styling

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../layouts/Header";
 import AddPost from "../components/AddPost";
+import Posts from "../components/Posts";
 
 const Home = () => {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Header />
       <AddPost />
+      <Posts />
     </div>
   );
 };
